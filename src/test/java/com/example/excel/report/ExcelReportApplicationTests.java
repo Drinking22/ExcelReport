ff31fa44400415aaae4400182250eae5e4663c7e
Drinking22 <166421630+Drinking22@users.noreply.github.com>
2025-03-19 08:06:23 +0300

    init

---
+++ src/test/java/com/example/excel/report/ExcelReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excel.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
